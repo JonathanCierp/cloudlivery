d7772573846839acad8015ee85690fbd288fe3c3
Jonathan CIERP <cierp.jonathan@gmail.com>
2020-05-11 01:08:07 +0200

    feat(frontend): effect on search bar and menu

--- frontend/components/core/CoreHeader.vue
+++ frontend/components/core/CoreHeader.vue
@@ -9,14 +9,14 @@
 			</div>
 			<div class="flex-1 mx-10 relative header__container__middle">
 				<img class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center" src="@/assets/img/icons/search.svg" alt="Logo du site">
-				<input class="bg-gray-200 shadow appearance-none rounded w-full py-2 px-4 pl-16 text-gray-700 leading-tight" placeholder="Oeuf, poisson etc...">
+				<input class="transition-colors duration-400 ease-in-out bg-gray-200 shadow appearance-none rounded w-full py-2 px-4 pl-16 text-gray-700 leading-tight focus:shadow-outline" placeholder="Oeuf, poisson etc...">
 			</div>
 			<div class="flex items-center justify-center header__container__right">
-				<nuxt-link class="inline-block px-4" to="/">
+				<nuxt-link class="inline-block px-4 hover:opacity-75" to="/">
 					<img class="inline px-1" src="@/assets/img/icons/user.svg" alt="Logo du site">
 					<span class="align-middle">Mon compte</span>
 				</nuxt-link>
-				<nuxt-link class="inline-block px-4" to="/">
+				<nuxt-link class="inline-block px-4 hover:opacity-75" to="/">
 					<img class="inline" src="@/assets/img/icons/cart.svg" alt="Logo du site">
 				</nuxt-link>
 			</div>
