d99d4a52fd98fb1be8a71f406e09f1b54725ff56
Ananas <j.cierp@xefi.fr>
2020-06-15 18:00:20 +0200

    fix(frontend) : add icons components and reset.css edit

---
+++ frontend/components/icons/IconClose.vue
@@ -0,0 +1,20 @@
+<template>
+	<svg id="icon-close" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M19.063 5.948a1.037 1.037 0 00-1.47 0L12.5 11.03 7.406 5.937a1.038 1.038 0 10-1.469 1.47l5.094 5.093-5.094 5.094a1.04 1.04 0 001.47 1.468L12.5 13.97l5.094 5.094a1.04 1.04 0 001.773-.735 1.038 1.038 0 00-.305-.734L13.97 12.5l5.094-5.094a1.044 1.044 0 000-1.458z"
+			fill="currentColor"/>
+	</svg>
+</template>
+
+<script>
+	export default {
+		name: "icon-close"
+	}
+</script>
+
+<style scoped>
+	svg {
+		width: 25px;
+		height: 25px;
+	}
+</style>
