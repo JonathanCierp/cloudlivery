bb30cd150a3b1b0aba65b8687282ada011c733d1
Jonathan CIERP <cierp.jonathan@gmail.com>
2020-07-07 00:05:54 +0200

    frontend : scraping carrefour

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>
-		<nuxt-link to="/a">
-			a
-		</nuxt-link>
+  <div class="container mx-auto my-5 flex item-center justify-center flex-wrap">
+		<div v-for="item in scraping.carrefour" class="bg-white m-2 shadow w-1/5">
+			<div>
+				<span class="block">{{ item.attributes.title }}</span>
+				<span class="block">{{ item.attributes.packaging }}</span>
+				<span class="block">{{ item.attributes.price.perUnitLabel }}</span>
+			</div>
+			<div>
+				<img :src="`https://www.carrefour.fr${item.attributes.images[0].largest.replace('size', '')}`">
+			</div>
+			<div>
+				<img width="30" :src="`https://www.carrefour.fr/images/badges/flag-common-poulepleinair.svg`">
+			</div>
+			<div>
+				<span class="block">{{ item.attributes.price.price.replace(".", ",") }}€</span>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-export default {
+	import GqlScrappingCarrefour from "@/utils/apollo/query/scrappingCarrefour"
 
-}
+	export default {
+		apollo: {
+			scraping: {
+				query: GqlScrappingCarrefour
+			}
+		},
+		data() {
+			return {
+				scraping: {}
+			}
+		}
+	}
 </script>
