edee3885cae221fec0c4267e70ce6db1e4cd5849
Jonathan CIERP <cierp.jonathan@gmail.com>
2020-09-09 00:08:23 +0200

    feat(backend) : users module, model

---
+++ backend/src/modules/users/users.model.ts
@@ -0,0 +1,37 @@
+import { ObjectType, Field } from "@nestjs/graphql"
+import { Entity, Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn } from "typeorm"
+
+@ObjectType()
+@Entity("users")
+export class UsersModel {
+	@Field({nullable: true})
+	@PrimaryGeneratedColumn()
+	id: number
+
+	@Field()
+	@Column({length: 255, unique: true})
+	google_id: string
+
+	@Field()
+	@Column({length: 255, unique: true})
+	email: string
+
+	@Field({nullable: true})
+	@Column({length: 255, nullable: true})
+	prefix_url: string
+
+	@Field({nullable: true})
+	@Column()
+	@UpdateDateColumn({
+		type: "timestamp",
+		default: () => "CURRENT_TIMESTAMP(6)",
+		onUpdate: "CURRENT_TIMESTAMP(6)",
+		nullable: true
+	})
+	public updatedAt?: Date
+
+	@Field({nullable: true})
+	@Column()
+	@CreateDateColumn({type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", nullable: true})
+	public createdAt?: Date
+}
